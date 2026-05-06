893641c2ab879ac72b4e992eaa2452746d0f76a7
jonatha <jonatha.sviegas@hotmail.com>
2026-05-06 15:53:45 -0300

    airflow docker com bq finalizado

--- docker/dags/orquestrador.py
+++ docker/dags/orquestrador.py
@@ -1,18 +1,35 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.operators.python import PythonOperator
-from src.extract_and_load import main_extract
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
 
 with DAG(
-
     dag_id='coin_gecko',
-    start_date=datetime(2024,1,1),
-    schedule="*/5 * * * *",
+    start_date=datetime(2024, 1, 1),
+    schedule="*/15 * * * *",
     catchup=False
+) as dag:
 
-    ) as dag:
-
-    extract_load_bq = PythonOperator(
+    extract_load_bq = DockerOperator(
         task_id='extract_load_bq',
-        python_callable=main_extract
-        )
+        image='extract_and_load:1.5',
+        api_version="auto",
+        auto_remove="success",
+        command="python3 extract_and_load.py",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+
+        mounts=[
+            Mount(
+                source="/home/jhon/Documentos/bigquery_key.json",
+                target="/opt/bigquery_key.json",
+                type="bind"
+            )
+        ],
+
+        environment={
+            "GOOGLE_APPLICATION_CREDENTIALS": "/opt/bigquery_key.json"
+        },
+
+        mount_tmp_dir=False
+    )
